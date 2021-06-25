ffe320fc9e3ade2274fb2dab030e6819e2be1dcb
jesussantana <jesus@esmarketingdigital.com>
2021-06-25 06:43:27 +0200

    Update class AutoMLClassifier

---
+++ scripts/tydy.py
@@ -0,0 +1,13 @@
+# Correlation between numeric columns
+# ==============================================================================
+def tidy_corr_matrix(corr_mat):
+    
+    # Function to convert a pandas correlation matrix to tidy format
+    
+    corr_mat = corr_mat.stack().reset_index()
+    corr_mat.columns = ['variable_1','variable_2','r']
+    corr_mat = corr_mat.loc[corr_mat['variable_1'] != corr_mat['variable_2'], :]
+    corr_mat['abs_r'] = np.abs(corr_mat['r'])
+    corr_mat = corr_mat.sort_values('abs_r', ascending=False)
+    
+    return(corr_mat)
